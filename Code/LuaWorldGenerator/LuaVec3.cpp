ffe71d4d3a54ccb06fb8c59908ea03123409fd75
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2022-12-30 07:19:58 -0700

    Replicated 90% of all vec3 functions

--- Code/LuaWorldGenerator/LuaVec3.cpp
+++ Code/LuaWorldGenerator/LuaVec3.cpp
@@ -13,6 +13,16 @@ extern "C"
 }
 
 #include <glm.hpp>
+#include <gtx\rotate_vector.hpp>
+
+/*
+vec3 {
+	bezier2 = function,
+	bezier3 = function,
+	closestAxis = function,
+	getRotation = function,
+}
+*/
 
 namespace SM
 {
@@ -81,13 +91,148 @@ namespace SM
 			return 1;
 		}
 
+		int Vec3::Mul(lua_State* L) //can be used with integers
+		{
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			switch (Base::Type(L, 2))
+			{
+			case LUA_TNUMBER:
+				{
+					const float v_number = static_cast<float>(lua_tonumber(L, 2));
+
+					glm::vec3* v_new_vec = CreateVec3Internal(L);
+					(*v_new_vec) = (*v_vec1) * v_number;
+
+					return 1;
+				}
+			case LUA_TSMVEC3:
+				{
+					glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+					glm::vec3* v_new_vec = CreateVec3Internal(L);
+					(*v_new_vec) = (*v_vec1) * (*v_vec2);
+
+					return 1;
+				}
+			}
+
+			G_LUA_CUSTOM_ARG_TYPE_ERROR(L, 2, LUA_TSMVEC3);
+		}
+
+		int Vec3::Div(lua_State* L) //can be used with integers
+		{
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			switch (Base::Type(L, 2))
+			{
+			case LUA_TNUMBER:
+				{
+					const float v_number = static_cast<float>(lua_tonumber(L, 2));
+
+					glm::vec3* v_new_vec = CreateVec3Internal(L);
+					(*v_new_vec) = (*v_vec1) / v_number;
+
+					return 1;
+				}
+			case LUA_TSMVEC3:
+				{
+					glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+					glm::vec3* v_new_vec = CreateVec3Internal(L);
+					(*v_new_vec) = (*v_vec1) / (*v_vec2);
+
+					return 1;
+				}
+			}
+
+			G_LUA_CUSTOM_ARG_TYPE_ERROR(L, 2, LUA_TSMVEC3);
+		}
+
+		int Vec3::Add(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = (*v_vec1) + (*v_vec2);
+
+			return 1;
+		}
+
+		int Vec3::Sub(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = (*v_vec1) - (*v_vec2);
+
+			return 1;
+		}
+
+		int Vec3::Length(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 1);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+
+			lua_pushnumber(L, static_cast<lua_Number>(glm::length(*v_vec1)));
+			return 1;
+		}
+
+		int Vec3::Length2(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 1);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			const float v_length = (v_vec1->x * v_vec1->x) + (v_vec1->y * v_vec1->y) + (v_vec1->z * v_vec1->z);
+
+			lua_pushnumber(L, static_cast<lua_Number>(v_length));
+			return 1;
+		}
+
 		int Vec3::Normalize(lua_State* L)
 		{
 			G_LUA_CUSTOM_ARG_CHECK(L, 1);
 			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+
+			glm::vec3* v_vec = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			if (glm::length(*v_vec) < 0.00000011920929f)
+				return luaL_error(L, "Vector must not be of length zero");
 		
 			glm::vec3* v_new_vec = CreateVec3Internal(L);
-			(*v_new_vec) = glm::normalize(*reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1)));
+			(*v_new_vec) = glm::normalize(*v_vec);
+
+			return 1;
+		}
+
+		int Vec3::SafeNormalize(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+
+			glm::vec3* v_vec = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+
+			if (glm::length(*v_vec) < 0.00000011920929f)
+			{
+				lua_pushvalue(L, 2); //push the fallback vector as the return value
+				return 1;
+			}
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::normalize(*v_vec);
 
 			return 1;
 		}
@@ -104,6 +249,136 @@ namespace SM
 
 			glm::vec3* v_new_vec = CreateVec3Internal(L);
 			(*v_new_vec) = glm::cross(*v_vec1, *v_vec2);
+
+			return 1;
+		}
+
+		int Vec3::Lerp(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 3);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 3, LUA_TNUMBER);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+			const float v_lerp_val = static_cast<float>(lua_tonumber(L, 3));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::mix(*v_vec1, *v_vec2, v_lerp_val);
+
+			return 1;
+		}
+		
+		int Vec3::Dot(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+			lua_pushnumber(L, static_cast<lua_Number>(glm::dot(*v_vec1, *v_vec2)));
+			return 1;
+		}
+
+		int Vec3::Min(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::min(*v_vec1, *v_vec2);
+
+			return 1;
+		}
+
+		int Vec3::Max(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			glm::vec3* v_vec2 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::max(*v_vec1, *v_vec2);
+
+			return 1;
+		}
+
+		int Vec3::Rotate(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 3);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TNUMBER);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 3, LUA_TSMVEC3);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			const float v_rotate_angle = static_cast<float>(lua_tonumber(L, 2));
+			glm::vec3* v_normal = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 3));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::rotate(*v_vec1, v_rotate_angle, *v_normal);
+
+			return 1;
+		}
+
+		int Vec3::RotateX(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TNUMBER);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			const float v_rotate_angle = static_cast<float>(lua_tonumber(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::rotateX(*v_vec1, v_rotate_angle);
+
+			return 1;
+		}
+
+		int Vec3::RotateY(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TNUMBER);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			const float v_rotate_angle = static_cast<float>(lua_tonumber(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::rotateY(*v_vec1, v_rotate_angle);
+
+			return 1;
+		}
+
+		int Vec3::RotateZ(lua_State* L)
+		{
+			G_LUA_CUSTOM_ARG_CHECK(L, 2);
+
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 1, LUA_TSMVEC3);
+			G_LUA_CUSTOM_ARG_TYPE_CHECK(L, 2, LUA_TNUMBER);
+
+			glm::vec3* v_vec1 = reinterpret_cast<glm::vec3*>(lua_touserdata(L, 1));
+			const float v_rotate_angle = static_cast<float>(lua_tonumber(L, 2));
+
+			glm::vec3* v_new_vec = CreateVec3Internal(L);
+			(*v_new_vec) = glm::rotateZ(*v_vec1, v_rotate_angle);
 
 			return 1;
 		}
@@ -209,8 +484,18 @@ namespace SM
 
 		static const std::unordered_map<std::string, int (*)(lua_State*)> g_vec3Userdata =
 		{
-			{ "normalize", Vec3::Normalize },
-			{ "cross"    , Vec3::Cross     }
+			{ "normalize"    , Vec3::Normalize     },
+			{ "safeNormalize", Vec3::SafeNormalize },
+			{ "cross"        , Vec3::Cross         },
+			{ "length"       , Vec3::Length        },
+			{ "length2"      , Vec3::Length2       },
+			{ "dot"          , Vec3::Dot           },
+			{ "min"          , Vec3::Min           },
+			{ "max"          , Vec3::Max           },
+			{ "rotate"       , Vec3::Rotate        },
+			{ "rotateX"      , Vec3::RotateX       },
+			{ "rotateY"      , Vec3::RotateY       },
+			{ "rotateZ"      , Vec3::RotateZ       }
 		};
 
 		int Vec3::Index(lua_State* L)
@@ -245,8 +530,20 @@ namespace SM
 				Table::PushFunction(L, "new", Lua::Vec3::New);
 				Table::PushFunction(L, "zero", Lua::Vec3::Zero);
 				Table::PushFunction(L, "one", Lua::Vec3::One);
+
 				Table::PushFunction(L, "normalize", Lua::Vec3::Normalize);
+				Table::PushFunction(L, "safeNormalize", Lua::Vec3::SafeNormalize);
 				Table::PushFunction(L, "cross", Lua::Vec3::Cross);
+				Table::PushFunction(L, "length", Lua::Vec3::Length);
+				Table::PushFunction(L, "length2", Lua::Vec3::Length2);
+				Table::PushFunction(L, "lerp", Lua::Vec3::Lerp);
+				Table::PushFunction(L, "dot", Lua::Vec3::Dot);
+				Table::PushFunction(L, "min", Lua::Vec3::Min);
+				Table::PushFunction(L, "max", Lua::Vec3::Max);
+				Table::PushFunction(L, "rotateX", Lua::Vec3::RotateX);
+				Table::PushFunction(L, "rotateY", Lua::Vec3::RotateY);
+				Table::PushFunction(L, "rotateZ", Lua::Vec3::RotateZ);
+				Table::PushFunction(L, "rotate", Lua::Vec3::Rotate);
 
 				Table::PushFunction(L, "getX", Lua::Vec3::GetX);
 				Table::PushFunction(L, "getY", Lua::Vec3::GetY);
@@ -269,6 +566,11 @@ namespace SM
 				Table::PushFunction(L, "__newindex", Lua::Vec3::NewIndex);
 				Table::PushFunction(L, "__index", Lua::Vec3::Index);
 				Table::PushFunction(L, "__tostring", Lua::Vec3::ToString);
+
+				Table::PushFunction(L, "__add", Lua::Vec3::Add);
+				Table::PushFunction(L, "__sub", Lua::Vec3::Sub);
+				Table::PushFunction(L, "__mul", Lua::Vec3::Mul);
+				Table::PushFunction(L, "__div", Lua::Vec3::Div);
 
 				lua_pop(L, 1); //pop the metatable off the stack
 			}
--- Code/LuaWorldGenerator/LuaVec3.hpp
+++ Code/LuaWorldGenerator/LuaVec3.hpp
@@ -12,16 +12,30 @@ namespace SM
 		namespace Vec3
 		{
 			int New(lua_State* L);
-
 			int Zero(lua_State* L);
-
 			int One(lua_State* L);
 
 			int Equals(lua_State* L);
 
-			int Normalize(lua_State* L);
+			int Mul(lua_State* L);
+			int Div(lua_State* L);
+			int Add(lua_State* L);
+			int Sub(lua_State* L);
 
+			int Length(lua_State* L);
+			int Length2(lua_State* L);
+			int Normalize(lua_State* L);
+			int SafeNormalize(lua_State* L);
 			int Cross(lua_State* L);
+			int Lerp(lua_State* L);
+			int Dot(lua_State* L);
+			int Min(lua_State* L);
+			int Max(lua_State* L);
+
+			int Rotate(lua_State* L);
+			int RotateX(lua_State* L);
+			int RotateY(lua_State* L);
+			int RotateZ(lua_State* L);
 
 			int GetX(lua_State* L);
 			int GetY(lua_State* L);
@@ -34,7 +48,6 @@ namespace SM
 			int ToString(lua_State* L);
 
 			int NewIndex(lua_State* L);
-
 			int Index(lua_State* L);
 
 			void Register(lua_State* L);
