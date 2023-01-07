4a4b7ae45bfb5e85e4526d2b405f80b61d50fff5
QuestionableM <77170113+QuestionableM@users.noreply.github.com>
2023-01-07 06:44:15 -0700

    Added simdjson

--- Code/Utils/Json.cpp
+++ Code/Utils/Json.cpp
@@ -112,3 +112,176 @@ std::string JsonReader::WriteJsonString(const nlohmann::json& v_json)
 
 	return v_str.str();
 }
+
+//Simdjson functions
+
+std::string JsonReader::RemoveComments(const std::string& json_string)
+{
+	std::string v_output;
+	v_output.reserve(json_string.size());
+
+	const char* const v_data_beg = json_string.data();
+	const char* const v_data_end = v_data_beg + json_string.size();
+
+	const char* v_data = v_data_beg;
+
+	std::size_t v_data_ptr = 0;
+	while (v_data != v_data_end)
+	{
+		if (*v_data == '\"')
+		{
+			v_data = strchr(v_data + 1, '\"');
+			if (!v_data) break;
+		}
+		else if (*v_data == '/')
+		{
+			const char* v_last_char = v_data++;
+			if (v_data == v_data_end)
+			{
+				break;
+			}
+			else if (*v_data == '/')
+			{
+				v_output.append(json_string.begin() + v_data_ptr, json_string.begin() + (v_last_char - v_data_beg));
+
+				v_data = strchr(v_data, '\n');
+				if (!v_data) break;
+
+				v_data_ptr = v_data - v_data_beg;
+				continue;
+			}
+			else if (*v_data == '*')
+			{
+				v_output.append(json_string.begin() + v_data_ptr, json_string.begin() + (v_last_char - v_data_beg));
+
+				v_data = strstr(v_data, "*/");
+				if (!v_data) break;
+
+				v_data_ptr = (v_data += 2) - v_data_beg;
+				continue;
+			}
+		}
+
+		v_data++;
+	}
+
+	if (v_data)
+	{
+		v_output.append(json_string.begin() + v_data_ptr, json_string.begin() + (v_data - v_data_beg));
+	}
+	else
+	{
+		v_output.append(json_string.substr(v_data_ptr));
+	}
+
+	return v_output;
+}
+
+bool JsonReader::LoadParseSimdjson(const std::wstring& path, simdjson::dom::document& v_doc)
+{
+	try
+	{
+		std::string v_json_str;
+		if (!File::ReadToString(path, v_json_str))
+			return false;
+
+		simdjson::dom::parser v_parser;
+		v_parser.parse_into_document(v_doc, v_json_str);
+
+		return true;
+	}
+#ifdef QENGINE_DEBUG
+	catch (const simdjson::simdjson_error& v_err)
+	{
+		DebugErrorL("Couldn't parse: ", path, "\nError: ", v_err.what());
+	}
+#else
+	catch (...) {}
+#endif
+
+	return false;
+}
+
+bool JsonReader::LoadParseSimdjsonC(const std::wstring& path, simdjson::dom::document& v_doc, const simdjson::dom::element_type& type_check)
+{
+	try
+	{
+		std::string v_json_str;
+		if (!File::ReadToString(path, v_json_str))
+			return false;
+
+		simdjson::dom::parser v_parser;
+		const auto v_root = v_parser.parse_into_document(v_doc, v_json_str);
+		if (v_root.type() != type_check)
+		{
+			DebugErrorL("Mismatching root json type!\nFile: ", path);
+			return false;
+		}
+
+		return true;
+	}
+#ifdef QENGINE_DEBUG
+	catch (const simdjson::simdjson_error& v_err)
+	{
+		DebugErrorL("Couldn't parse: ", path, "\nError: ", v_err.what());
+	}
+#else
+	catch (...) {}
+#endif
+
+	return false;
+}
+
+bool JsonReader::LoadParseSimdjsonComments(const std::wstring& path, simdjson::dom::document& v_doc)
+{
+	try
+	{
+		std::string v_json_str;
+		if (!File::ReadToString(path, v_json_str))
+			return false;
+
+		v_json_str = JsonReader::RemoveComments(v_json_str);
+
+		simdjson::dom::parser v_parser;
+		v_parser.parse_into_document(v_doc, v_json_str);
+
+		return true;
+	}
+#ifdef QENGINE_DEBUG
+	catch (const simdjson::simdjson_error& v_err)
+	{
+		DebugErrorL("Couldn't parse: ", path, "\nError: ", v_err.what());
+	}
+#else
+	catch (...) {}
+#endif
+
+	return false;
+}
+
+bool JsonReader::LoadParseSimdjsonCommentsC(const std::wstring& path, simdjson::dom::document& v_doc, const simdjson::dom::element_type& type_check)
+{
+	try
+	{
+		std::string v_json_str;
+		if (!File::ReadToString(path, v_json_str))
+			return false;
+
+		v_json_str = JsonReader::RemoveComments(v_json_str);
+
+		simdjson::dom::parser v_parser;
+		const auto v_root = v_parser.parse_into_document(v_doc, v_json_str);
+
+		return true;
+	}
+#ifdef QENGINE_DEBUG
+	catch (const simdjson::simdjson_error& v_err)
+	{
+		DebugErrorL("Couldn't parse: ", path, "\nError: ", v_err.what());
+	}
+#else
+	catch (...) {}
+#endif
+
+	return false;
+}
--- Code/Utils/Json.hpp
+++ Code/Utils/Json.hpp
@@ -1,6 +1,15 @@
 #pragma once
+#pragma unmanaged
 
-#include <nlohmann/json.hpp>
+#include <nlohmann\json.hpp>
+
+#if defined(_MT)
+	#undef _MT
+	#include <simdjson\simdjson.h>
+	#define _MT
+#else
+	#include <simdjson\simdjson.h>
+#endif
 
 class JsonReader
 {
@@ -31,6 +40,62 @@ public:
 		return m_emptyObject;
 	}
 
+	//Simdjson functions
+
+	//Only used by Simdjson as it doesn't support json with comments
+	static std::string RemoveComments(const std::string& json_string);
+
+	//Parse simdjson unchecked
+	static bool LoadParseSimdjson(const std::wstring& path, simdjson::dom::document& v_doc);
+	//Parse simdjson with root node type check
+	static bool LoadParseSimdjsonC(const std::wstring& path, simdjson::dom::document& v_doc, const simdjson::dom::element_type& type_check);
+	//Parse simdjson with comments
+	static bool LoadParseSimdjsonComments(const std::wstring& path, simdjson::dom::document& v_doc);
+	//Parse simdjson with comments and root node type checking
+	static bool LoadParseSimdjsonCommentsC(const std::wstring& path, simdjson::dom::document& v_doc, const simdjson::dom::element_type& type_check);
+
+	//Should be used to get numbers from simdjson elements
+	template<typename T>
+	inline constexpr static T GetNumber(const simdjson::simdjson_result<simdjson::dom::element>& v_elem)
+	{
+		static_assert(std::is_arithmetic_v<T>, "Json::GetNumber -> Template argument must be of arithmetic type!");
+
+		switch (v_elem.type())
+		{
+		case simdjson::dom::element_type::DOUBLE:
+			{
+				if constexpr (std::is_same_v<T, double>)
+					return v_elem.get_double();
+				else
+					return static_cast<T>(v_elem.get_double());
+			}
+		case simdjson::dom::element_type::INT64:
+			{
+				if constexpr (std::is_same_v<T, long long>)
+					return v_elem.get_int64();
+				else
+					return static_cast<T>(v_elem.get_int64());
+			}
+		case simdjson::dom::element_type::UINT64:
+			{
+				if constexpr (std::is_same_v<T, unsigned long long>)
+					return v_elem.get_uint64();
+				else
+					return static_cast<T>(v_elem.get_uint64());
+			}
+		}
+
+		return 0;
+	}
+
 private:
 	inline const static nlohmann::json m_emptyObject = nlohmann::json();
+
+	JsonReader() = default;
+	JsonReader(const JsonReader&&) = delete;
+	JsonReader(JsonReader&&)       = delete;
+	JsonReader(JsonReader&)        = delete;
+	~JsonReader() = default;
 };
+
+#pragma managed
